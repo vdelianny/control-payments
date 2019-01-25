b4822cda4e4425a44e99d2f26babdf657c354aab
Delianny <deliannyv@gmail.com>
2019-01-24 22:11:58 -0430

    indica cuando el estudiante debe un mes - valor del mes estático

--- src/public/js/paymentsMatch.js
+++ src/public/js/paymentsMatch.js
@@ -1,5 +1,12 @@
-const paymentsMatch = function (payments) {
-    return true;
+const paymentsMatch = function (payments, number) {
+	lastPayment = payments[payments.length - 1];
+
+	//consulta si el último mes pagado es menor al mes que se quiere consultar, retorna true en caso de deuda
+
+	if (lastPayment.month < number) {
+    	return true;
+	}
+	return false;
 }
 
 module.exports = paymentsMatch;
